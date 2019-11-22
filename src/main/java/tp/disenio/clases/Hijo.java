467647b84b1576e0eae23d72544beeb7120f50e4
claraMazzi <mazzimclara@gmail.com>
2019-11-22 15:35:12 -0300

    CLARA

--- src/main/java/tp/disenio/clases/Hijo.java
+++ src/main/java/tp/disenio/clases/Hijo.java
@@ -2,18 +2,13 @@ package tp.disenio.clases;
 
 public class Hijo {
 
-	public int id_hijo;
+	
 	public String FechaNac;
 	public String estadoCivil;
 	public String Sexo;
 
 
-	public int getId_hijo() {
-		return id_hijo;
-	}
-	public void setId_hijo(int id_hijo) {
-		this.id_hijo = id_hijo;
-	}
+	
 	public String getFechaNac() {
 		return FechaNac;
 	}
--- src/main/java/tp/disenio/clases/TipoCobertura.java
+++ src/main/java/tp/disenio/clases/TipoCobertura.java
@@ -3,6 +3,7 @@ package tp.disenio.clases;
 import tp.disenio.clases.Usuario;
 
 public class TipoCobertura {
+	
 	private float porcentajeActual;
 	private String idUsuario;
 	private String nombre;
